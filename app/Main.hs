0af0747c415d9941e9bb327710b80de86f628235
Shozo Shinmura <the.little.schemer@gmail.com>
2018-12-21 23:30:33 +0900

    src/Turtle.hs の修正
      + runTurtle のリファクタリング
      + circleR, circleL, drawCircle を追加

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,11 @@ import Turtle
 
 
 main :: IO ()
-main = display window white pic
-  where
-    window = InWindow "Turtle" (800, 600) (10, 10)
-    pic    = Pictures $ snd $ runTurtle (0, (0, 0), black, True) cmds
-    cmds   = take 250 $ concat [[forward n, right 93] | n <- iterate (* 1.05) 2]
+main = do
+  display window white $ Pictures [pic1, pic2, Line [(0, 200), (0, -200)], Line [(-200, 0), (200, 0)]]
+    where
+      window = InWindow "Turtle" (800, 600) (10, 10)
+      (_, pic1) = circleR 100 (0, (0, 0), red, True)
+      (_, pic2) = circleL 100 (0, (0, 0), blue, True)
+    -- cmds = concat $ replicate 36 [forward 50, right 10]
+    -- pic = snd $ runTurtle cmds (0, (0, 0), red, True)
--- src/Turtle.hs
+++ src/Turtle.hs
@@ -7,13 +7,16 @@ import Graphics.Gloss
 -- 亀の状態 : (向き, 位置, 色, Pen (up / down))
 type TurtleST = (Float, Point, Color, Bool)
 
+-- コマンド
+type Command = TurtleST -> (TurtleST, Picture)
+
 
 --
 -- Turtle Commands
 --
 
 -- n だけ前進する
-forward :: Float -> TurtleST -> (TurtleST, Picture)
+forward :: Float -> Command
 forward n (h, (x, y), c, pen) = ((h, p, c, pen), pic)
   where
     h'  = h * pi / 180
@@ -21,49 +24,70 @@ forward n (h, (x, y), c, pen) = ((h, p, c, pen), pic)
     pic = if pen then (Color c $ Line [(x, y), p]) else Blank
 
 -- n だけ後退する
-backward :: Float -> TurtleST -> (TurtleST, Picture)
+backward :: Float -> Command
 backward n tST = forward (- n) tST
 
 -- th 度だけ左旋回する
-left :: Float -> TurtleST -> (TurtleST, Picture)
+left :: Float -> Command
 left th (h, p, c, pen) = ((h + th, p, c, pen), Blank)
 
 -- th 度だけ右旋回する
-right :: Float -> TurtleST -> (TurtleST, Picture)
+right :: Float -> Command
 right th (h, p, c, pen) = ((h - th, p, c, pen), Blank)
 
 -- p へ移動する
-goto :: Point -> TurtleST -> (TurtleST, Picture)
+goto :: Point -> Command
 goto p (h, p', c, pen) = ((h, p, c, pen), pic)
   where pic = if pen then (Color c $ Line [p', p]) else Blank
 
 -- 移動時に線を描く
-penDown :: TurtleST -> (TurtleST, Picture)
+penDown :: Command
 penDown (h, p, c, _) = ((h, p, c, True), Blank)
 
 -- 移動時に線を描かない
-penUp :: TurtleST -> (TurtleST, Picture)
+penUp :: Command
 penUp (h, p, c, _) = ((h, p, c, False), Blank)
 
 -- 線の色を設定する
-setColor :: Color -> TurtleST -> (TurtleST, Picture)
+setColor :: Color -> Command
 setColor c (h, p, _, pen) = ((h, p, c, pen), Blank)
 
 -- 亀の位置を設定する
-setPoint :: Point -> TurtleST -> (TurtleST, Picture)
+setPoint :: Point -> Command
 setPoint p (h, _, c, pen) = ((h, p, c, pen), Blank)
 
 -- 亀の向きを設定する
-setAngle :: Float -> TurtleST -> (TurtleST, Picture)
+setAngle :: Float -> Command
 setAngle th (_, p, c, pen) = ((th, p, c, pen), Blank)
 
 
 --
 -- runTurtle
 --
-runTurtle :: TurtleST
-          -> [TurtleST -> (TurtleST, Picture)]
-          -> (TurtleST, [Picture])
-runTurtle tST cmdList = foldl f (tST, []) cmdList
-  where f (tST, pList) cmd = (tST', pic : pList)
-          where (tST', pic) = cmd tST
+runTurtle :: [Command] -> Command
+runTurtle cmdLst tST = loop cmdLst tST []
+  where
+    loop [] tST picLst = (tST, Pictures picLst)
+    loop (cmd : cmdLst) tST picLst = loop cmdLst tST' (pic : picLst)
+      where (tST', pic) = cmd tST
+
+
+-- 円を描く
+tCircle :: (Float -> Command) -> Float -> Command
+tCircle cmd r tST = runTurtle cmdLst tST
+  where
+    n = 2 * r * sin (5 * pi / 180)
+    cmdLst = concat $ replicate 36 [cmd 5, forward n, cmd 5]
+
+-- 半径 r の右回りの円を描く
+circleR :: Float -> Command
+circleR r tST = tCircle right r tST
+
+-- 半径 r の左回りの円を描く
+circleL :: Float -> Command
+circleL r tST = tCircle left r tST
+
+-- 亀の位置を中心に半径 r の円を描く
+drawCircle :: Float -> Command
+drawCircle r tST@(_, (x, y), c, _) = (tST, pic)
+  where pic = Translate x y $ Color c $ Circle r
