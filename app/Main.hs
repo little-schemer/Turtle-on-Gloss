dfab9c11d80ed317c0eeeecfbc726a65539e0347
Shozo Shinmura <the.little.schemer@gmail.com>
2018-12-21 23:38:26 +0900

    app/Main.hs の修正

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,8 @@ import Turtle
 
 main :: IO ()
 main = do
-  display window white $ Pictures [pic1, pic2, Line [(0, 200), (0, -200)], Line [(-200, 0), (200, 0)]]
+  display window white $ Pictures [pic1, pic2]
     where
       window = InWindow "Turtle" (800, 600) (10, 10)
       (_, pic1) = circleR 100 (0, (0, 0), red, True)
       (_, pic2) = circleL 100 (0, (0, 0), blue, True)
-    -- cmds = concat $ replicate 36 [forward 50, right 10]
-    -- pic = snd $ runTurtle cmds (0, (0, 0), red, True)
