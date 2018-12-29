4ffe9d01177825ad3b7ab86806d3f4ac808f5ad3
Shozo Shinmura <the.little.schemer@gmail.com>
2018-12-29 13:56:29 +0900

    app/koch/Main.hs を追加
    app/koch/Main.hs : 再帰による画像描画

--- app/koch/Main.hs
+++ app/koch/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Graphics.Gloss
 import Turtle
 
-
+-- | コッホ曲線
 koch :: Float -> Int -> [Command]
 koch len 0 = [forward len]
 koch len n = koch' ++ lt ++ koch' ++ rt ++ koch' ++ lt ++ koch'
@@ -16,9 +16,5 @@ koch len n = koch' ++ lt ++ koch' ++ rt ++ koch' ++ lt ++ koch'
 main :: IO ()
 main = display window white pic
     where
-      window = InWindow "Turtle" (800, 600) (10, 10)
+      window = InWindow "Koch Curve" (800, 600) (10, 10)
       (_, pic) = runTurtle (koch 400 4) initST { point = (-200, 0)}
-
-      -- st = TurtleST {angle = 0, point = (0, 0), penColor = white, pen = True}
-      -- cs = (left 9) : (concat $ replicate 12 [drawPolygonL 20 30, left 30])
-      -- (_, pic) = runTurtle cs st
