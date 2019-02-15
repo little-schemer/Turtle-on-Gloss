f66afcc3d0421f80b38bd5933adf4f176a767d60
Shozo Shinmura <the.little.schemer@gmail.com>
2019-02-15 17:19:25 +0900

    sample/sandbox/Main.hs : drawArcL, drawArcR の実験

--- sample/sandbox/Main.hs
+++ sample/sandbox/Main.hs
@@ -8,18 +8,43 @@ import Debug.Trace
 
 
 main :: IO ()
-main = display window white $ pic1 <> pic2 <> pic4
+main = display window white $ pic1 <> pic2 <> pic3
     where
       window   = InWindow "Sandbox" (800, 600) (10, 10)
       pic1 = Color red $ Line [(-400, 0), (400, 0)]
       pic2 = Color red $ Line [(0, -400), (0, 400)]
-      -- pic3 = snd $ runTurtle [left 30, fd 100, drawArcL 100 90] initST
-      pic4 = Arc 90 370 100       -- start end 半径
+      pic3 = snd $ runTurtle [left 300, fd 100, drawArcL 90 100, lt 90, drawArcL 90 100] initST
+      -- pic4 = Arc 90 370 100       -- start end 半径
+
+-- drawArcL :: Float -> Float -> Command
+-- drawArcL r th st = (st', Translate ox oy $ Rotate (180 - a - th) $ isDraw st $ Arc 0 th r)
+--   where
+--     a = angle st
+--     (ox, oy) = newPoint r (a + 90) (point st)
+--     st' = st {angle = a + th, point = newPoint r (a + th - 90) (ox, oy)}
+
+-- drawArcR :: Float -> Float -> Command
+-- drawArcR r th st = (st', Translate ox oy $ Rotate (th - a - 90) $ isDraw st $ Arc 0 th r)
+--   where
+--     a = angle st
+--     (ox, oy) = newPoint r (a - 90) (point st)
+--     st' = st {angle = a - th, point = newPoint r (a - th + 90) (ox, oy)}
 
 
--- drawArc :: (Float -> Command) -> Float -> Float -> Command
--- drawArc cmd r th st = (st, Translate ox oy $ Rotate (90 - angle st) $ Arc 0 th r)
---   where (ox, oy) = newPoint r $ fst (cmd 90 st)
 
--- drawArcL = drawArc left
--- drawArcR = drawArc right
+
+drawArcL :: Float -> Float -> Command
+drawArcL th r st = (st', Translate ox oy $ isDraw st $ Arc a' (a' + th) r)
+  where
+    a  = angle st
+    a' = a - 90
+    (ox, oy) = newPoint r (a + 90) (point st)
+    st' = st {angle = a + th, point = newPoint r (a + th - 90) (ox, oy)}
+
+drawArcR :: Float -> Float -> Command
+drawArcR th r st = (st', Translate ox oy $ isDraw st $ Arc a' (a' + th) r)
+  where
+    a  = angle st
+    a' = 90 + a - th
+    (ox, oy) = newPoint r (a - 90) (point st)
+    st' = st {angle = a - th, point = newPoint r (a - th + 90) (ox, oy)}
