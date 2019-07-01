86b1ef0cc917d544620e3de2ba1b9a7c33a3da1e
Shozo Shinmura <the.little.schemer@gmail.com>
2019-07-01 16:39:58 +0900

    modify app/Main.hs .

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,4 @@ main :: IO ()
 main = display window white pic
     where
       window = InWindow "Triangle" (800, 600) (10, 10)
-      (pic, _) = runTurtle (take 5 $ cycle [forward 100, left 120]) initST
+      (pic, _) = runTurtle (take 5 $ cycle [forward 150, left 120]) initST
