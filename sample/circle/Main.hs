d529195a81d959f7641b0a6821b74536bd8d5f74
Shozo Shinmura <the.little.schemer@gmail.com>
2020-01-26 20:10:01 +0900

    Update : sample/circle/Main.hs
    * sample/circle/Main.hs
      + src/Graphics/Turtle.hs の変更に伴なう修正。

--- sample/circle/Main.hs
+++ sample/circle/Main.hs
@@ -10,4 +10,4 @@ main = runTurtle window (greyN 0.3) 50 tLst
     window = InWindow "Circle" (800, 600) (10, 10)
     col   = [setColor c | c <- cycle [red, green, blue]]
     angle = [setAngle th | th <- [30, 60 .. 360]]
-    tLst  = [(initST, [a, b] ++ drawPolygonL 60 10) | (a, b) <- zip col angle]
+    tLst  = [(initST, [a, b, drawPolygonL 60 10]) | (a, b) <- zip col angle]
