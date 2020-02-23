cc83d1513509ba2501a6474c4a9e9cc91bb9439f
Shozo Shinmura <the.little.schemer@gmail.com>
2020-02-23 17:10:34 +0900

    Update : sample の circle.hs を circleDance.hs に差し替え。

---
+++ sample/circleDance.hs
@@ -0,0 +1,33 @@
+------------------------------------------------------------
+-- 色を変化させながら、複数の円を同時に描く
+------------------------------------------------------------
+
+import Graphics.Gloss
+import Graphics.Turtle
+
+
+main :: IO ()
+main = runTurtle windows black 50 tLst
+  where windows = InWindow "Circle Dance" (800, 600) (10, 10)
+
+cols :: [Command]
+cols = [setColor c | c <- cycle [col1, col2, col3, col4, col5, col6]]
+  where
+    col1 = makeColor 0.0 0.0 0.2 1
+    col2 = makeColor 0.0 0.2 0.0 1
+    col3 = makeColor 0.0 0.2 0.2 1
+    col4 = makeColor 0.2 0.0 0.0 1
+    col5 = makeColor 0.2 0.0 0.2 1
+    col6 = makeColor 0.2 0.2 0.0 1
+
+angles :: [Command]
+angles = [setAngle th | th <- [30, 60 .. 360]]
+
+circleDance :: Command
+circleDance = repCommand 3 [circle', lt 10]
+  where
+    circle' = repCommand 36 [drawArcL 10 100, updateColor f f f id]
+    f x = x * 1.016
+
+tLst :: [(TurtleST, [Command])]
+tLst = [(initST, [a, b] ++ [circleDance]) | (a, b) <- zip cols angles]
