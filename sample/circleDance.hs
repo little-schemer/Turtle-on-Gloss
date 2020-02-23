6518e5ea8a2de8d23c96f1933259337f0f5c001c
Shozo Shinmura <the.little.schemer@gmail.com>
2020-02-23 17:47:05 +0900

    Update : README.org を修正。

--- sample/circleDance.hs
+++ sample/circleDance.hs
@@ -13,21 +13,21 @@ main = runTurtle windows black 50 tLst
 cols :: [Command]
 cols = [setColor c | c <- cycle [col1, col2, col3, col4, col5, col6]]
   where
-    col1 = makeColor 0.0 0.0 0.2 1
-    col2 = makeColor 0.0 0.2 0.0 1
-    col3 = makeColor 0.0 0.2 0.2 1
-    col4 = makeColor 0.2 0.0 0.0 1
-    col5 = makeColor 0.2 0.0 0.2 1
-    col6 = makeColor 0.2 0.2 0.0 1
+    col1 = makeColor 0.0 0.0 0.1 1
+    col2 = makeColor 0.0 0.1 0.1 1
+    col3 = makeColor 0.0 0.1 0.0 1
+    col4 = makeColor 0.1 0.1 0.0 1
+    col5 = makeColor 0.1 0.0 0.0 1
+    col6 = makeColor 0.1 0.0 0.1 1
 
 angles :: [Command]
-angles = [setAngle th | th <- [30, 60 .. 360]]
+angles = [setAngle th | th <- [30, 90 .. 360]]
 
 circleDance :: Command
-circleDance = repCommand 3 [circle', lt 10]
+circleDance = repCommand 6 [circle', lt 10]
   where
     circle' = repCommand 36 [drawArcL 10 100, updateColor f f f id]
-    f x = x * 1.016
+    f x = x * 1.012
 
 tLst :: [(TurtleST, [Command])]
 tLst = [(initST, [a, b] ++ [circleDance]) | (a, b) <- zip cols angles]
