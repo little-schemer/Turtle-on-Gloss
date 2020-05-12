6080d43824a74864d565a1f7027036f298daebb9
Shozo Shinmura <the.little.schemer@gmail.com>
2020-05-12 16:52:37 +0900

    Update : app/Main.hs で drawPolygon を使用するように変更。

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
-import Graphics.Gloss
-import Graphics.Turtle
+import           Graphics.Gloss
+import           Graphics.Turtle
 
 main :: IO ()
-main = runTurtle initWindow black 20 [(st, [flower 100])]
+main = runTurtle initWindow black 50 [(st, [flower 100])]
   where st = initST {point = (0, -250), angle = 90}
 
 
@@ -13,9 +13,14 @@ flower n = concat [ setColor green
                   , fd n, pu, fd n, pd, flower' n
                   ]
   where
-    flower' n = concat [setColor rose, cmd (n / 2), setColor yellow, cmd (n / 6)]
-      where cmd n = repCommand 12 [lt 30, drawArcL 360 n]
+    flower' n = concat [ setColor rose, cmd1 (n / 2)
+                       , setColor yellow, lt 30, cmd2 (n / 3)
+                       , setColor orange, lt 60, cmd2 (n / 3) ]
+      where
+        cmd1 n = repCommand 12 [lt 30, drawArcL 360 n]
+        cmd2 n = concat
+                 $ replicate 3
+                 $ drawPolygon [fd n, rt 120, fd n, rt 120, fd n]
 
     leaf n = concat [ setColor green, fd (n * 1.5), rt 135
-                    , drawArcR 90 n , rt 90, drawArcR 90 n
-                    ]
+                    , drawArcR 90 n , rt 90, drawArcR 90 n ]
